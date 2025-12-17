e641b2603e36bc38a34533c2b09370aa38e53623
Stepan Badalyan <stepanbadalyan8@gmail.com>
2025-12-18 00:55:44 +0400

    Added: General files

---
+++ test-task-app/src/App.js
@@ -0,0 +1,14 @@
+import './assets/scss/index.scss';
+import Business from "./assets/js/pages/business";
+import Utilities from "./assets/utilities";
+
+function App() {
+  return (
+    <div className="container">
+        <Utilities/>
+        <Business />
+    </div>
+  );
+}
+
+export default App;
